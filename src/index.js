f8703f2666d3f6ad0f748d1a711ede0b48a82476
201930232 <jeonjuseong11@gmail.com>
2023-03-30 12:24:34 +0900

    Add : Clock.jsx
    시계 예제 만들어보기

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
+//import Library from './chapter_3/Library';
 import reportWebVitals from './reportWebVitals';
-import Library from './chapter_3/Library';
+import Clock from './chapter_4/Clock';
+setInterval(()=>{
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>
-);
+)},1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
