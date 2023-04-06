80d999c76d40e21d77656ce1f49efa5dc1fd2fa9
201930232 <jeonjuseong11@gmail.com>
2023-04-06 12:45:25 +0900

    Add : Notification
    6주차 진행

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import NotificationList from './chapter_06/NotificationList';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CommentList from './chapter_05/CommentList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>
 );
 
