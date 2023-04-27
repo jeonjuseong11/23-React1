6dcffaea0df76d3aed8207886b3cac8f7341e367
201930232 <jeonjuseong11@gmail.com>
2023-04-27 11:24:41 +0900

    Add : chapter_08
    chapter_08 클릭 이벤트 처리 실습

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 
