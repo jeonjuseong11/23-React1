7bad18506d9c44281ff97c72428e02c889d1d1d1
201930232 <jeonjuseong11@gmail.com>
2023-05-04 12:30:39 +0900

    Fix : SignUp.jsx

--- src/chapter_11/SignUp.jsx
+++ src/chapter_11/SignUp.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 
 const SignUp = (props) => {
   const [name, setName]=useState("");
+  const [gender, setGender]=useState("");
 
   const handleChangeName = (e) => {
     setName(e.target.value);
   }
+  const handleChangeGender = (e) => {
+    setGender(e.target.value);
+  }
   const handleSubmit = (e) => {
-    alert(`이름: ${name}`);
+    alert(`이름: ${name} 성별: ${gender}`);
     e.preventDefault();
   }
   return (
@@ -15,6 +19,13 @@ const SignUp = (props) => {
       <label>
         이름:
         <input type="text" value={name} onChange={handleChangeName} />
+      </label>
+      <label>
+        성별:
+        <select value={gender} onChange={handleChangeGender}>
+          <option value="남자">남자</option>
+          <option value="여자">여자</option>
+        </select>
       </label>
       <button type='submit'>제출</button>
     </form>
