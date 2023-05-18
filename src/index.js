0710ed23ad9f450e4d7aa0922a5156405d8e924b
201930232 <jeonjuseong11@gmail.com>
2023-05-18 10:59:19 +0900

    Add : chapter_13
    13장 Card 컴포넌트 만들기 실습

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Calculator from './chapter_12/Calculator';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ProfileCard from "./chapter_13/ProfileCard";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Calculator />
-  </React.StrictMode>
+    <ProfileCard />
+  </React.StrictMode>,
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
