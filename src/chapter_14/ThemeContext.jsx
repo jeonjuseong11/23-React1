eff50faf7f9086af1bc5d58e5871d686701d7841
201930232 <jeonjuseong11@gmail.com>
2023-05-25 12:26:44 +0900

    Add : chapter_14
    Context 사용 실습

---
+++ src/chapter_14/ThemeContext.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const ThemeContext = React.createContext();
+ThemeContext.displayName = "ThemeContenxt";
+
+export default ThemeContext;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ProfileCard from "./chapter_13/ProfileCard";
+import DarkOrLight from "./chapter_14/DarkOrLight";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ProfileCard />
+    <DarkOrLight />
   </React.StrictMode>,
 );
 
